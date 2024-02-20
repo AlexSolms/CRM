a78db48b9bff247991d2f9691e0988187ac50f96
Alex Solms <duaedin@web.de>
2024-02-20 22:11:08 +0100

    button added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTooltipModule
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
