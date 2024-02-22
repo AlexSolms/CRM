ed1cbffe592182206d41a919367fd3e7f96c17c4
Alex Solms <duaedin@web.de>
2024-02-22 12:28:42 +0100

    add progress bar

--- src/app/diaglog/add-user/add-user.component.ts
+++ src/app/diaglog/add-user/add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {
   MatDialogTitle,
@@ -7,15 +7,17 @@ import {
   MatDialogClose,
   MatDialogRef,
 } from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../../models/user.class';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
+import { Firestore, collection, collectionData, addDoc, updateDoc } from '@angular/fire/firestore';
 
 
 
@@ -34,23 +36,49 @@ import { FormsModule } from '@angular/forms';
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatProgressBarModule
   ],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.scss'
 })
 export class AddUserComponent {
 
+  firestore: Firestore = inject(Firestore);
+
   user: User = new User();
   birthDate!: Date;
+  loading: boolean = false;
 
-  constructor(public dialogRef: MatDialogRef<AddUserComponent>) {  }
+  constructor(public dialogRef: MatDialogRef<AddUserComponent>) {
+    console.log('huhu', this.getNotesRef());
+  }
 
+  //schließt den Dialog, wenn ich daneben klicke
   onNoClick(): void {
     this.dialogRef.close();
   }
-  save(){
+
+  async save() {
+    this.loading=true;
     this.user.birthDate = this.birthDate.getTime();
     console.log(this.user);
+    let getRef = collection(this.firestore, 'user');
+    await addDoc(getRef, this.user.toJson()).catch(
+      (err) => { console.error(err) }
+    ).then(
+      (result: any) => {
+        console.log("Document written with ID: ", result)
+      });
+
+    /* this.firestore.collection('user').add(this.user).then((result:any){
+
+    }); */
+    this.loading=false;
+    this.dialogRef.close();
+  }
+  async getNotesRef() {
+    return collection(this.firestore, 'user');
+
   }
 }
