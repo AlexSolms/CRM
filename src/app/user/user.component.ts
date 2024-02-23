1453119109dc079a8fd07d4d1c99b7d679e9ef83
Alex Solms <duaedin@web.de>
2024-02-23 10:56:58 +0100

    table added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,8 +4,28 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatDialogModule, MatDialog} from '@angular/material/dialog';
 import { AddUserComponent } from '../diaglog/add-user/add-user.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
 
+export interface PeriodicElement {
+  name: string;
+  //position: number;
+  email: number;
+  city: string;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Hydrogen', email: 1.0079, city: 'H'},
+  {name: 'Helium', email: 4.0026, city: 'He'},
+  {name: 'Lithium', email: 6.941, city: 'Li'},
+  {name: 'Beryllium', email: 9.0122, city: 'Be'},
+  {name: 'Boron', email: 10.811, city: 'B'},
+  {name: 'Carbon', email: 12.0107, city: 'C'},
+  {name: 'Nitrogen', email: 14.0067, city: 'N'},
+  {name: 'Oxygen', email: 15.9994, city: 'O'},
+  {name: 'Fluorine', email: 18.9984, city: 'F'},
+  {name: 'Neon', email: 20.1797, city: 'Ne'},
+];
 
 @Component({
   selector: 'app-user',
@@ -15,13 +35,19 @@ import { AddUserComponent } from '../diaglog/add-user/add-user.component';
     MatButtonModule,
     MatTooltipModule,
     MatDialogModule,
+    MatCardModule,
+    MatTableModule
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
   constructor(public dialog: MatDialog) {}
-
+  displayedColumns: string[] = ['name', 'email', 'city'];
+  dataSource = ELEMENT_DATA;
+  seeMore(){
+    
+  }
   openDialog(){
     this.dialog.open(AddUserComponent);
 
